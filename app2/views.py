032dbccb62f4775854e2faa62342f13df0dc14c7
Jose Sepulveda <jose.sepulveda105@inacapmail.cl>
2022-09-26 14:55:00 -0300

    app 2 casi lista

--- app2/views.py
+++ app2/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+
+def tiempo(request):
+    dt=datetime.datetime.now()
+    s="<h1>fecha y hora actual : </h1>"+str(dt)
+    return HttpResponse
+
--- jose_proyecto1/urls.py
+++ jose_proyecto1/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.app1 import views
+from django.app2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
