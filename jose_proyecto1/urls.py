176bce112693380c03a1e92b17abef6408721a02
Jose Sepulveda <jose.sepulveda105@inacapmail.cl>
2022-09-26 15:09:08 -0300

    app2 terminada

--- jose_proyecto1/urls.py
+++ jose_proyecto1/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.app1 import views
 from django.app2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mensaje',views.texto),
     path('reloj/',views.tiempo)
 ]
